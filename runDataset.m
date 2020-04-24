047fc6f083aa7ee837ce450d703926a921486c96
Ravdeep <rpasr001@ucr.edu>
2020-04-24 02:58:30 -0700

    cleaned up directory structure

--- runDataset.m
+++ runDataset.m
@@ -1,8 +1,8 @@
 % FilePath to the dataset and  ground truth labels and you need to change
 % the X and Y variable below(look at the line number 8-10)
-datasetFname = 'dataset/Indian_pines_corrected.mat';
-datasetGt = 'dataset/Indian_pines_gt.mat';
-outFile = 'IndianPines'; % Update this name for different datasets accordingly.
+datasetFname = 'dataset/SalinasA_corrected.mat';
+datasetGt = 'dataset/SalinasA_gt.mat';
+outFile = 'SalinasA'; % Update this name for different datasets accordingly.
 
 datasetOut = 'tensorDataset';
 
@@ -10,24 +10,19 @@ data = load(datasetFname);
 gt = load(datasetGt);
 
 % IMPORTANT: Change X and Y according to variable stored in the .mat(dataset) file
-% X = data.salinasA_corrected;
-% Y =  gt.salinasA_gt;
-X = data.indian_pines_corrected;
-Y = gt. indian_pines_gt;
-folderPath = strcat(datasetOut, '/', outFile);
-outFname = strcat(folderPath, '/', outFile);
-if ~exist(folderPath, 'dir')
-    disp(folderPath);
-    mkdir(convertStringsToChars(folderPath));
-end
-
-
-
+X = data.salinasA_corrected;
+Y =  gt.salinasA_gt;
+% X = data.indian_pines_corrected;
+% Y = gt. indian_pines_gt;
 
 % train and test split - if you want to use 80-20 split(80% training and 20%
 % testing), assign testSize=0.2, if you want 30-70 split(30% training and
 % 70% tesing) assign testSize=0.7
-testSize = 0.7;
+testSize = 0.2;
+
+a = testSize * 100;
+b = 100 - a;
+splitName = strcat(string(b), string(a));
 
 % Number of datasets to be created
 % numData = 10;
@@ -41,6 +36,12 @@ numData = 2;
 ranks = [10 20];
 
 
+folderPath = strcat(datasetOut, '/', splitName, '/', outFile);
+outFname = strcat(folderPath, '/', outFile);
+if ~exist(folderPath, 'dir')
+    disp(folderPath);
+    mkdir(convertStringsToChars(folderPath));
+end
 
 for r=ranks
     for i=1:numData
